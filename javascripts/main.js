7bd9fa4ada66040b23501f8da9d904e3e0e63d29
kaikaimeng <kaikai.meng@gmail.com>
2014-07-06 14:48:14 +0800

    test

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,17 +3,9 @@
 		dataType: 'json',
 		url: 'http://booklog.io/1/post',
 	    success: function(response) {
-			var content = $('#content');
-			var posts = response.posts;		// array
-			var html = '';
-
-			posts.forEach(function (post) {
-				html += '<div class="alert alert-success">';
-				html += post.subject;
-				html += '</div>' 
-			});
-
-			content.html(html);
+			$('#postTemplate')
+				.tmpl(response.posts)
+				.appendTo('#content');
 	    }
 	});
 }) ($);
